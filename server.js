69a34b7cef8f40d5399615459069cea7f20e57f7
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-01 08:06:27 +0000

    Use middlewares and set register route

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const home = require('./routes/home');
+const register = require('./routes/register')
 const path = require('path');
 const PORT = 8080;
 
 const app = express()
-
-app.use('/', home);
-
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hjs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended : false}))
+app.use('/', home);
+app.use('/register', register)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, (err) => {
