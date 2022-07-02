bfeb18904281a3b79843fe43ce95954cc9d4874e
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-02 20:50:52 +0000

    Fix

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const register = require('./routes/register');
 const login = require('./routes/login');
 const mongoose = require('mongoose');
 const path = require('path');
+const MongoDB_URL = "mongodb+srv://desmond:desmond@cluster0.xcbpx.mongodb.net/?retryWrites=true&w=majority";
 
 // connection to database 
-mongoose.connect("mongodb://localhost/users_storage", (err, data) => {
+mongoose.connect(MongoDB_URL || "mongodb://localhost/users_storage", (err, data) => {
     if (err) {
         console.log(err);
         return
@@ -26,5 +27,5 @@ app.use('/login', login),
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT || 8080, (err) => {
-    if (err) console.log(err);;
+    if (err) console.log(err);
 });
