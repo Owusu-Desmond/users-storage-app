0c229c31836d600040a918600567bcbe7736fe09
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-03 20:59:04 +0000

    Login user if user exist

--- routes/login.js
+++ routes/login.js
@@ -2,25 +2,39 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 
-// create a response for the homepage
-router.post('/', (req, res , next) => {
-    User.create(req.body, (err ,user) => {
-        if(err){
+// create a post response after login user
+router.post('/', (req, res, next) => {
+    // check if user already exist
+    const email = req.body.email;
+    const password = req.body.password;
+    User.find({ email: email }, (err, users) => {
+        if (err) {
             res.json(
-              {
-                configuration: 'fail',
-                error : err,
-            }  
-            );
+                {
+                    configuration: "fail",
+                    error: err,
+                }
+            )
             return;
         }
-        // when there is no error
-        res.json({
-            configuration: "Success",
-            user : user,
-        })
+        // login validation
+        if (users.length === 0) {
+            const data = {
+                emailError: "Email address is not registered!",
+                email: email,
+            }
+            res.render('login', data)
+        } else if (password !== users[0].password) {
+            const data = {
+                passwordError: "Password do not match with email!",
+                email: email,
+            }
+            res.render('login', data)
+        }else{
+            res.send(`Hello ${users[0].username}, you are almost there, we are in the process of creating your storage for you.`)
+        }
     })
-}) 
+})
 
 router.get("/", (req, res, next) => {
     res.render("login", null);
